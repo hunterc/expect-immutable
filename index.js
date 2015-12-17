4f49f5882da7b68514c8932ee151d5384e14f8d3
Hunter Cassidy <hunter@mobelux.com>
2015-12-17 11:47:37 -0500

    add eslint config

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import expect from 'expect';
 import { Iterable } from 'immutable';
 
+function ensureIterable(object) {
+  expect.assert(
+    Iterable.isIterable(object),
+    'expected %s to be an Immutable Iterable',
+    object
+  );
+}
+
 const api = {
   toEqualImmutable(iterable) {
     ensureIterable(this.actual);
@@ -16,13 +24,5 @@ const api = {
     expect(this.actual.toJS()).toNotEqual(iterable.toJS());
   }
 };
-
-function ensureIterable(object) {
-  expect.assert(
-    Iterable.isIterable(object),
-    'expected %s to be an Immutable Iterable',
-    object
-  );
-}
 
 export default api;
